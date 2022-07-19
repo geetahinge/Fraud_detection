9db61d88de4d33c8afba36029f459c76ceac42a2
geetahinge <geetahinge@gmail.com>
2022-07-19 12:44:04 +0530

    Added ML file to branch

--- Data_processing.py
+++ Data_processing.py
@@ -1,12 +1,1 @@
-from calendar import c
-
-
 print("Adding all data processing code related to Fraud detection")
-
-a=7
-b=8
-
-def sum(a,b): 
-     a+b 
-     return c
-print("added a new function")
